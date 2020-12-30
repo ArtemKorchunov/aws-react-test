d8f29371b3e9c24113dce4d37511513923a13b8f
Artem Korchunov <artem.korchunov@smava.de>
2020-12-30 15:53:53 +0200

    Change desc

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          AWS REACT CRA
         </a>
       </header>
     </div>
